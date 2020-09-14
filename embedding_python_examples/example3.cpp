114332cca6bd2a742904cd539bb9b2e548fe3633
t-cann <tom.cann@live.co.uk>
2020-09-14 10:20:52 +0100

    Finished - Changed Functionality

--- embedding_python_examples/example3.cpp
+++ embedding_python_examples/example3.cpp
@@ -1,19 +1,16 @@
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
-//References:
-//https://www.linuxjournal.com/article/8497
-//Not finished 
+//References: https://www.linuxjournal.com/article/8497
+
 
 /**
- * @brief 
+ * @brief Opens a file and runs a file, loop over command certain number of times.
  * 
  * @param filename 
  * @param num 
  * @param exp 
  */
-void process_expression(char* filename,
-                        int num,
-                        char* exp[])
+void process_expression(char* filename, int num, char* command) //, char* exp)
 {
     FILE*       exp_file;
 
@@ -25,18 +22,22 @@ void process_expression(char* filename,
     // functions to be made available
     // to user expressions
     exp_file = fopen(filename, "r");
-    PyRun_SimpleFile(exp_file, exp[0]);
+    PyRun_SimpleFile(exp_file, "exp"); // Don't understand relevance of second argument here. Does not effect result if changed. 
 
-    // Iterate through the expressions
-    // and execute them
+    // Iterate through the expressions and execute them
     while(num--) {
-        PyRun_SimpleString(*exp++);
+        PyRun_SimpleString(command);
         PyRun_SimpleString("print(x)");
     }
 }
 
 /**
- * @brief 
+ * @brief Run a file and runs a command. 
+ * 
+ * e.g. ./example3 helloworld.py x+=10 
+ *   Hello World!
+ *   10
+ *   20
  * 
  * @param argc 
  * @param argv 
@@ -47,9 +48,9 @@ int main(int argc, char* argv[])
     Py_Initialize();
 
     if(argc != 3) {
-        printf("Usage: %s FILENAME EXPRESSION+\n");
+        printf("Usage: %s FILENAME EXPRESSION+\n", argv[0]);
         return 1;
     }
-    process_expression(argv[1], argc - 1, argv + 2);
+    process_expression(argv[1], argc - 1, argv[2]);
     return 0;
 }

d7a71ec0c9c8d9285b68fb9e2ac4dd7c66b25410
t-cann <tom.cann@live.co.uk>
2020-09-14 10:40:46 +0100

    Examples of parseTuple

--- embedding_python_examples/example5.cpp
+++ embedding_python_examples/example5.cpp
@@ -1,33 +1,77 @@
 #define PY_SSIZE_T_CLEAN  /* Make "s#" use Py_ssize_t rather than int. */
 #include <Python.h>
+// https://docs.python.org/2.0/ext/parseTuple.html
 
 /**
- * @brief Not Finshed - Program that converts C-Type Array into Python Variable. 
+ * @brief Program examples of using parseTuple . 
  * 
  */
+
+int main(int argc, const char* argv[])
+{  
+PyObject* args;
 
 int ok;
 int i, j;
 long k, l;
-const char *s;
-Py_ssize_t size;
+char *s;
+int size;
+
+ok = PyArg_ParseTuple(args, ""); /* No arguments */ 
+/* Python call: f() */
+    
+
+ok = PyArg_ParseTuple(args, "s", &s); /* A string */
+    /* Possible Python call: f('whoops!') */
+
+ok = PyArg_ParseTuple(args, "lls", &k, &l, &s); /* Two longs and a string */
+    /* Possible Python call: f(1, 2, 'three') */
+
+ok = PyArg_ParseTuple(args, "(ii)s#", &i, &j, &s, &size);
+    /* A pair of ints and a string, whose size is also returned */
+    /* Possible Python call: f((1, 2), 'three') */
+
+
+char *file;
+const char *mode = "r";
+int bufsize = 0;
+ok = PyArg_ParseTuple(args, "s|si", &file, &mode, &bufsize);
+/* A string, and optionally another string and an integer */
+/* Possible Python calls:
+    f('spam')
+    f('spam', 'w')
+    f('spam', 'wb', 100000) */
+
+
+int left, top, right, bottom, h, v;
+ok = PyArg_ParseTuple(args, "((ii)(ii))(ii)",
+            &left, &top, &right, &bottom, &h, &v);
+/* A rectangle and a point */
+/* Possible Python call:
+    f(((0, 0), (400, 300)), (10, 10)) */
+
+Py_complex c;
+ok = PyArg_ParseTuple(args, "D:myfunction", &c);
+/* a complex, also providing a function name for errors */
+/* Possible Python call: myfunction(1+2j) */
 
 //PyObject* tuple = PyTuple_New();
 //tuple = PyTuple_SetItem();
-    
-ok = PyArg_ParseTuple(args, ""); /* No arguments */
+return ok;
+}
 
+//Example of Better formated Running of Simple String
 
-    // PyRun_SimpleString(
-    //     "a_foo = None\n"
-    //     "\n"
-    //     "def setup(a_foo_from_cxx):\n"
-    //     "    print 'setup called with', a_foo_from_cxx\n"
-    //     "    global a_foo\n"
-    //     "    a_foo = a_foo_from_cxx\n"
-    //     "\n"
-    //     "def run():\n"
-    //     "    a_foo.doSomething()\n"
-    //     "\n"
-    //     "print 'main module loaded'\n"
-    // );
+// PyRun_SimpleString(
+//     "a_foo = None\n"
+//     "\n"
+//     "def setup(a_foo_from_cxx):\n"
+//     "    print 'setup called with', a_foo_from_cxx\n"
+//     "    global a_foo\n"
+//     "    a_foo = a_foo_from_cxx\n"
+//     "\n"
+//     "def run():\n"
+//     "    a_foo.doSomething()\n"
+//     "\n"
+//     "print 'main module loaded'\n"
+// );
